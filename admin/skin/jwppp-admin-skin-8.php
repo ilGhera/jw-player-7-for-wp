4b9caa32ac8e3e47bc958329c349a6d2b253582a
ilGhera <andrea@ilghera.com>
2019-05-06 14:40:41 +0200

    Main file activation function Go Premium function Main admin options JW Player 8 skin options

--- admin/skin/jwppp-admin-skin-8.php
+++ admin/skin/jwppp-admin-skin-8.php
@@ -17,96 +17,104 @@ require( JWPPP_ADMIN . 'skin/jwppp-admin-skin-8-options.php' );
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Constrolbar Text', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-text" value="' . esc_attr( $jwppp_skin_color_controlbar_text ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-text" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Color of plain text in the control bar, such as the time.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Constrolbar Icons', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-icons" value="' . esc_attr( $jwppp_skin_color_controlbar_icons ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-icons" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Default, inactive color of all icons in the control bar.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Constrolbar Active Icons', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-active-icons" value="' . esc_attr( $jwppp_skin_color_controlbar_active_icons ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-active-icons" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Color of hovered or selected icons in the control bar.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Constrolbar Background', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-background" value="' . esc_attr( $jwppp_skin_color_controlbar_background ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-controlbar-background" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Background color of the control bar and the volume slider. Default background is transparent..', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Timeslider Progress', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-timeslider-progress" value="' . esc_attr( $jwppp_skin_color_timeslider_progress ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-timeslider-progress" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Color of the progress bar in the time slider.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Timeslider Rail', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-timeslider-rail" value="' . esc_attr( $jwppp_skin_color_timeslider_rail ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-timeslider-rail" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Base color of the timeslider, known as the rail.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Menus Text', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-menus-text" value="' . esc_attr( $jwppp_skin_color_menus_text ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-menus-text" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Color of inactive, default text in menus and the Next Up overlay.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Menus Active Text', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-menus-active-text" value="' . esc_attr( $jwppp_skin_color_menus_active_text ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-menus-active-text" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Color of hovered or selected text in menus. ', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Menus Background', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-menus-background" value="' . esc_attr( $jwppp_skin_color_menus_background ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-menus-background" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Background color of menus and the Next Up overlay.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Tooltips Text', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-tooltips-text" value="' . esc_attr( $jwppp_skin_color_tooltips_text ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-tooltips-text" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Color of tooltips text.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '<tr>';
 echo '<th scope="row">' . esc_html( __( 'Tooltips Background', 'jwppp' ) ) . '</th>';
 echo '<td>';
-echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-tooltips-background" value="' . esc_attr( $jwppp_skin_color_tooltips_background ) . '">';
+echo '<input type="text" class="jwppp-color-field" name="jwppp-skin-color-tooltips-background" disabled="disabled">';
 echo '<p class="description">' . esc_html( __( 'Background color of tooltips.', 'jwppp' ) ) . '</p>';
+go_premium();
 echo '</td>';
 echo '</tr>';
 
 echo '</table>';
 
-/*Add nonce to the form*/
-wp_nonce_field( 'jwppp-nonce-skin', 'hidden-nonce-skin' );
-
 echo '<input type="hidden" name="set" value="1" />';
-echo '<input class="button button-primary" type="submit" id="submit" value="' . esc_attr( __( 'Save chages', 'jwppp' ) ) . '">';
+echo '<input class="button button-primary" type="submit" id="submit" value="' . esc_attr( __( 'Save chages', 'jwppp' ) ) . '" disabled="disabled">';
 echo '</form>';
